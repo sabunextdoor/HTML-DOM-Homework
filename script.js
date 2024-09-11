3551f720c3dcaffdf4898030eaa0b3cb775a2d44
sabunextdoor <sabuthwala@gmail.com>
2024-09-11 18:27:19 +0200

    DOM Homework

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction {
+document.getElementById
+    document.getElementById("demo").innerHTML=
+    document.getElementById("result").firstChild.nodeValue;
+    document.getElementById("demo").innerHTML=
+    document.getElementById("reult1").childNodes[0].nodeValue;
+}
